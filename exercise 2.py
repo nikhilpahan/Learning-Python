d412bd3b5de7b21d1e9c89afdb18d365b1487ce9
nikhilpahan <nikhilpahan02@gmail.com>
2023-12-01 03:12:49 +0530

    Operator Done

--- exercise 2.py
+++ exercise 2.py
@@ -1,2 +1,11 @@
 message = "Hope your're having fun!"
 print("The message", message)
+
+
+answer = 5 + 2**3
+print("the answer is:", answer)
+
+result = 5 - 3
+#print("5 - 3 =", result)
+
+print("Done!")
